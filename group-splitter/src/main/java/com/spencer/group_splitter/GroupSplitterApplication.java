cde8f77cb399f2e73093d47eff752f64bb869ed6
sp3ncerlan <lansp329@gmail.com>
2025-06-16 15:28:38 -0500

    Initial commit

---
+++ group-splitter/src/main/java/com/spencer/group_splitter/GroupSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.spencer.group_splitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupSplitterApplication.class, args);
+	}
+
+}
---
+++ group-splitter/src/test/java/com/spencer/group_splitter/GroupSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spencer.group_splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
